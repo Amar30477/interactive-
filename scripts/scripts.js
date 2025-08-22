9f9077135649d40c88cea39e9c9b3de85b7186e3
amar <amar.makram2020@gmail.com>
2025-08-22 20:12:31 +0300

    interactive-rating

---
+++ scripts/scripts.js
@@ -0,0 +1,23 @@
+const button = document.querySelectorAll(".btn");
+const smt = document.querySelector("#submitbtn");
+const container= document.getElementsByClassName("container");
+const result = document.getElementsByClassName("resultcontainer");
+const rate = document.getElementsByClassName("reslt");
+let value;
+button.forEach((e)=>{
+    e.addEventListener("click",(e)=>{
+        button.forEach((r)=> r.classList.remove("active"));
+        e.target.classList.add("active");
+        value = e.target.innerText;
+        // console.log(value);
+    });
+        // console.log(e.target) ;
+});
+smt.addEventListener("click",()=>{
+    console.log("clicked");
+    console.log(value);
+    container[0].style.display="none" ;
+    result[0].style.display="block";
+    rate.textContent= `"${value.reslt}"`;
+});
+
